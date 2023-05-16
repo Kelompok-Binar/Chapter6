b9e48d6bbc59774707a91e6b513ba54ba7e41abd
AbyanSS <abyansurya04@gmail.com>
2023-05-17 00:41:46 +0700

    Update 1.2

--- Challenge_Chapter_6/src/main/java/com/example/challenge_chapter_4/Security/WebSecurityConfig.java
+++ Challenge_Chapter_6/src/main/java/com/example/challenge_chapter_4/Security/WebSecurityConfig.java
@@ -1,27 +1,33 @@
 package com.example.challenge_chapter_4.Security;
 
+import com.example.challenge_chapter_4.JWT.JwtAuthenticationFilter;
+import jakarta.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class WebSecurityConfig{
+    private JwtAuthenticationFilter jwtAuthFilter;
+    private AuthenticationProvider authenticationProvider;
+    private LogoutHandler logoutHandler;
+
     @Bean
     public UserDetailsService userDetailsService(){
 //        UserDetails admin = User.withUsername("diva").password("juan").roles("ADMIN").build();
@@ -31,14 +37,30 @@ public class WebSecurityConfig{
         return new UserInfoUserDetailsService();
     }
     @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.csrf()
+        httpSecurity.csrf()
                 .disable()
                 .authorizeHttpRequests()
                 .requestMatchers("/Film","/Jadwal","/Studio",
                         "/Film/Judul-Film/{film_name}","/Film/Tayang","/Film//Jadwal/{film_name}","/Seats/Studios/{studio}/{nomor_kursi}","/swagger-ui/**","/v3/api-docs/**", "/auth/**").permitAll()
-//                .anyRequest().authenticated().and().httpBasic() formLogin()
-                .and().authorizeHttpRequests().requestMatchers("/Seats/**","/Users/**","/Film/**","/Jadwal/**","/Report/**").authenticated().and().httpBasic().and().build();
+                .and().authorizeHttpRequests().requestMatchers("/Seats/**","/Users/**","/Film/**","/Jadwal/**","/Report/**").authenticated().and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout()
+                .logoutUrl("/auth/logout")
+                .addLogoutHandler(logoutHandler)
+                .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
+        return httpSecurity.build();
     }
 
     @Bean
@@ -50,13 +72,5 @@ public class WebSecurityConfig{
     //untuk encode password
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService());
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
     }
 }
